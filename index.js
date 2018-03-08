fda365851a8996d18b717d5be629881cfa2cbcfb
Thomas Urban <thomas.urban@cepharum.de>
2018-03-08 23:14:04 +0100

    revising eslint rules

--- index.js
+++ index.js
@@ -36,45 +36,143 @@ module.exports = {
 		mocha: true,
 		node: true,
 	},
-	extends: "eslint:recommended",
+	extends: [
+		"eslint:recommended",
+		"plugin:promise/recommended",
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			globalReturn: false,
+			impliedStrict: false,
+		},
+		ecmaVersion: 6,
+	},
 	plugins: [
 		"promise",
-		"standard"
 	],
 	rules: {
-		"array-bracket-spacing": [ "warn", "always", {
-			arraysInArrays: true,
-			objectsInArrays: true,
-			singleValue: false,
-		} ],
-		"comma-dangle": [ "warn", "only-multiline" ],
-		"comma-style": [ "warn", "last" ],
-		curly: [ "error", "multi-line" ],
-		"dot-notation": "warn",
+		"array-bracket-spacing": [
+			"warn",
+			"always",
+			{
+				singleValue: false,
+			}
+		],
+		"arrow-parens": [
+			"warn",
+			"as-needed"
+		],
+		"arrow-spacing": "warn",
+		"block-spacing": [
+			"warn",
+			"always"
+		],
+		"brace-style": [
+			"warn",
+			"1tbs",
+			{
+				allowSingleLine: true,
+			}
+		],
+		camelcase: [
+			"warn",
+			{
+				properties: "always",
+			}
+		],
+		"computed-property-spacing": "warn",
+		"consistent-return": "error",
+		"consistent-this": [
+			"warn",
+			"that"
+		],
+		curly: "off",
+		"default-case": "off",
+		"dot-location": "off",
+		"for-direction": "error",
+		"func-call-spacing": "warn",
+		"func-style": [
+			"warn",
+			"declaration",
+			{
+				allowArrowFunctions: true,
+			}
+		],
+		"getter-return": "error",
 		"guard-for-in": "error",
-		indent: [ "warn", "tab", {
-			SwitchCase: 1
-		} ],
+		indent: [
+			"warn",
+			"tab",
+			{
+				SwitchCase: 1,
+			}
+		],
 		"key-spacing": [ "warn", {
 			beforeColon: false,
-			mode: "strict"
+			mode: "strict",
 		} ],
-		"max-len": [ "warn", {
-			code: 200,
-			ignoreRegExpLiterals: true,
-		} ],
-		"max-nested-callbacks": [ "error", 5 ],
+		"linebreak-style": "warn",
+		"lines-around-comment": [
+			"warn",
+			{
+				allowArrayStart: true,
+				allowBlockStart: true,
+				allowClassStart: true,
+				allowObjectStart: true,
+				beforeBlockComment: true,
+			}
+		],
+		"lines-between-class-members": "warn",
+		"max-depth": [
+			"warn",
+			6,
+		],
+		"max-len": [
+			"warn",
+			{
+				code: 160,
+				ignoreRegExpLiterals: true,
+			}
+		],
+		"max-nested-callbacks": [
+			"error",
+			5
+		],
 		"new-cap": [ "error", {
 			capIsNew: false,
 		} ],
+		"no-array-constructor": "error",
+		"no-await-in-loop": "error",
+		"no-caller": "error",
+		"no-catch-shadow": "error",
+		"no-confusing-arrow": "error",
 		"no-duplicate-imports": "error",
 		"no-else-return": "warn",
+		"no-empty-function": "error",
+		"no-eval": "error",
+		"no-extend-native": "error",
 		"no-extra-bind": "error",
+		"no-extra-label": "error",
+		"no-extra-parens": [
+			"warn",
+			"all", {
+				conditionalAssign: false,
+				enforceForArrowConditionals: false,
+				nestedBinaryExpressions: false,
+				returnAssign: false,
+			}
+		],
 		"no-floating-decimal": "warn",
+		"no-implicit-coercion": "error",
+		"no-implied-eval": "error",
 		"no-labels": "error",
 		"no-lone-blocks": "error",
 		"no-lonely-if": "warn",
-		"no-mixed-spaces-and-tabs": [ "warn", "smart-tabs" ],
+		"no-mixed-operators": "error",
+		"no-mixed-spaces-and-tabs": [
+			"warn",
+			"smart-tabs"
+		],
 		"no-multiple-empty-lines": [ "warn", {
 			max: 4,
 			maxBOF: 0,
@@ -84,11 +182,15 @@ module.exports = {
 		"no-new-object": "error",
 		"no-new-wrappers": "error",
 		"no-proto": "error",
-		"no-return-assign": [ "warn", "except-parens" ],
+		"no-return-assign": "error",
 		"no-self-compare": "error",
-		"no-tabs": [0],
+		"no-shadow": "warn",
+		"no-template-curly-in-string": "error",
 		"no-throw-literal": "error",
-		"no-unmodified-loop-condition": "error",
+		"no-trailing-spaces": "warn",
+		"no-undef-init": "warn",
+		"no-unmodified-loop-condition": "warn",
+		"no-unneeded-ternary": "error",
 		"no-unused-expressions": [ "error", {
 			allowShortCircuit: true,
 		} ],
@@ -96,40 +198,81 @@ module.exports = {
 			args: "after-used",
 		} ],
 		"no-useless-call": "error",
+		"no-useless-computed-key": "error",
 		"no-useless-concat": "warn",
+		"no-useless-constructor": "error",
+		"no-useless-rename": "error",
 		"no-useless-return": "warn",
+		"no-var": "error",
 		"no-void": "error",
-		"no-warning-comments": "warn",
+		"no-whitespace-before-property": "warn",
 		"no-with": "error",
-		"object-curly-spacing": [ "warn", "always", {
-			arraysInObjects: true,
-			objectsInObjects: true
-		} ],
-		"one-var": 0,
-		"padded-blocks": 0,
+		"object-curly-newline": [
+			"warn",
+			{
+				consistent: true,
+			}
+		],
+		"object-curly-spacing": [
+			"warn",
+			"always"
+		],
+		"prefer-const": "warn",
+		"prefer-numeric-literals": "error",
 		"prefer-promise-reject-errors": "error",
+		"prefer-rest-params": "error",
+		"prefer-spread": "warn",
 		"quote-props": [ "warn", "as-needed" ],
-		quotes: [ "warn", "double" ],
+		quotes: [
+			"warn",
+			"double",
+			{
+				allowTemplateLiterals: true,
+				avoidEscape: true,
+			}
+		],
+		"require-await": "error",
 		"require-jsdoc": [ "error", {
 			require: {
 				ArrowFunctionExpression: false,
 				ClassDeclaration: true,
 				FunctionDeclaration: true,
+				FunctionExpression: false,
 				MethodDefinition: true,
 			}
 		} ],
-		semi: [ "error", "always" ],
-		"space-before-function-paren": [ "warn", "never" ],
-		"space-in-parens": [ "warn", "always" ],
-		"space-infix-ops": ["warn"],
-		strict: [ "warn", "global" ],
-		"valid-jsdoc": [ "error", {
-			requireParamDescription: false,
-			requireReturn: false,
-			requireReturnDescription: false,
-			requireReturnType: true,
-		} ],
+		"rest-spread-spacing": "warn",
+		semi: "error",
+		"semi-spacing": "warn",
+		"semi-style": "error",
+		"space-before-blocks": "warn",
+		"space-before-function-paren": [
+			"warn",
+			"never",
+		],
+		"space-in-parens": [
+			"warn",
+			"always", {
+				exceptions: [
+					"empty",
+				],
+			}
+		],
+		"space-infix-ops": "warn",
+		"spaced-comment": "warn",
+		strict: [
+			"error",
+			"global",
+		],
+		"switch-colon-spacing": [
+			"warn", {
+				after: true,
+				before: true,
+			}
+		],
+		"template-curly-spacing": "warn",
+		"valid-jsdoc": "error",
 		"wrap-iife": [ "error", "inside" ],
-		yoda: "warn",
+		yoda: "error"
 	}
 };
